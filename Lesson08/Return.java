db342c815477d7d65fb69028fa1b452202537363
shuymn <ndozto@gmail.com>
2017-07-16 01:51:14 +0900

    Learn about return statement

---
+++ Lesson08/Return.java
@@ -0,0 +1,41 @@
+class Car {
+    int num;
+    double gas;
+
+    int getNum() {
+        System.out.println("ナンバーを調べました．");
+        return num;
+    }
+
+    double getGas() {
+        System.out.println("ガソリン量を調べました．");
+        return gas;
+    }
+
+    void setNumGas(int n, double g) {
+        num = n;
+        gas = g;
+        System.out.println("車のナンバーを" + num + "に，ガソリン量を" + gas + "にしました．");
+    }
+
+    void show() {
+        System.out.println("車のナンバーは" + num + "です．");
+        System.out.println("ガソリン量は" + gas + "です．");
+    }
+}
+
+class Return {
+
+    public static void main(String[] args) {
+        Car car1 = new Car();
+
+        car1.setNumGas(1234, 20.5);
+
+        int number = car1.getNum();
+        double gasoline = car1.getGas();
+
+        System.out.println("サンプルから車を調べたところ");
+        System.out.println("ナンバーは" + number);
+        System.out.println("ガソリン量は" + gasoline + "でした．");
+    }
+}
